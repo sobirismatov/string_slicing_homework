dacdb1ca9c9c6c2eb23ddf5358583273dd26f77d
sobirismatov <name@uzcoder96@gmail.com>
2022-11-03 11:19:24 +0500

    08

--- Slicing08.py
+++ Slicing08.py
@@ -1,9 +1,10 @@
 def main(s):
-    """
-    The s string variable is given. return the characters in the odd position.
-    Args:
+    """  The s string variable is given. return the characters in the odd position.
+    A
+  rgs:
         s(str): parameter
     Returns:
-        str: answer
+        str: answer 12345933588
     """
-    return
+    return s[1::2]
+print(main("123456"))
